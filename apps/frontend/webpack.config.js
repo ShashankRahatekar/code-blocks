4b3057ae3e05636f124fd7b0079e59c4ebd223ff
Shashank R <rahateks@amdocs.com>
2025-04-19 10:34:31 +0530

    Init frontend

---
+++ apps/frontend/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['css-loader', 'style-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        })
+    ],
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+        historyApiFallback: true
+    },
+    mode: "development"
+};
