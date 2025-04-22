34e2391f5821db694886a0d5a174678db10f35bc
Shashank R <rahateks@amdocs.com>
2025-04-22 21:02:41 +0530

    CodeEditor common component added

--- apps/frontend/src/features/problems/pages/ProblemDetails.tsx
+++ apps/frontend/src/features/problems/pages/ProblemDetails.tsx
@@ -1,59 +1,21 @@
 import React, { useEffect, useReducer, useRef, useState } from "react";
 import {useParams} from 'react-router-dom';
 import ProblemService from '@services/problemsServices';
-import * as monaco from 'monaco-editor'
-// import styles from 'monaco-editor/'
+import CodeEditor from "@/components/CodeEditor";
 
 type ParamsT = {
     problemId: string
 };
-
 const ProblemDetailsPage = (props: any): React.JSX.Element => {
-    const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
     const params = useParams<ParamsT>();
-    const monacoEl = useRef(null);
     console.log(params);
-
-    self.MonacoEnvironment = {
-        getWorkerUrl: function (_moduleId, label) {
-            switch (label) {
-                case 'json':
-                    return '/json.worker.bundle.js';
-                case 'css':
-                case 'scss':
-                case 'less':
-                    return '/css.worker.bundle.js';
-                case 'html':
-                case 'handlebars':
-                case 'razor':
-                    return '/html.worker.bundle.js';
-                case 'typescript':
-                case 'javascript':
-                    return '/ts.worker.bundle.js';
-                default:
-                    return '/editor.worker.bundle.js';
-            }
-        }
-    };    
-
-    useEffect(() => {
-        if(monacoEl) {
-            setEditor(editor => {
-                if(editor) return editor;
-                return monaco.editor.create(monacoEl.current!, {
-                    value: 'function hello() {console.log("hello");}',
-                    language: 'javascript',
-                    theme: "default"
-                })
-            })
-        }
-    }, [monacoEl.current]);
 
     useEffect(() => {
         async function getProductDetails(): Promise<void> {
             if(params.problemId) {
                 const problem = await ProblemService.getProblem(params.problemId);
                 console.log({problem});
+                
             }
         }
         getProductDetails();
@@ -62,7 +24,7 @@ const ProblemDetailsPage = (props: any): React.JSX.Element => {
     return <div className="grid grid-cols-2 gap-4">
         <div> This is problem statement </div>
         <div> 
-            <div id="editor" className="Editor" ref={monacoEl}></div>
+            <CodeEditor code={`function hello() {\n console.log("hello");\n }`} />
         </div>
     </div>
 }
